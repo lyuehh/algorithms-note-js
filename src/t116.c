595d32cbaf966ab25cf34487720c05e285729c84
lyuehh <lyuehh@gmail.com>
2013-03-13 21:10:29 +0800

    add 1-16, 1-17

---
+++ src/t116.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int getline1(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main(int argc, const char *argv[])
+{
+  int len;
+  int max;
+  char line[MAXLINE];
+  char longest[MAXLINE];
+
+  max = 0;
+  while((len = getline1(line, MAXLINE)) > 0) {
+    printf("%d: %s", len, line);
+    if(len > max) {
+      max = len;
+      copy(longest, line);
+    }
+  }
+  if(max > 0) {
+    printf("Longest %d: %s", len, longest);
+  }
+  return 0;
+}
+
+int getline1(char s[], int lim) {
+  int c,i,j;
+  for(i=0,j=0;i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+    if(i < lim -1) {
+      s[j++] = c;
+    }
+  }
+  if(c == '\n') {
+    if(i <= lim - 1) {
+      s[j++] = c;
+    }
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+void copy(char to[], char from[]) {
+  int i;
+  i = 0;
+  while((to[i] = from[i]) != '\0') {
+    ++i;
+  }
+}
