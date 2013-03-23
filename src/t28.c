168e9864ab5fcd7f7e97b0082f19b60a56397646
lyuehh <lyuehh@gmail.com>
2013-03-23 17:04:30 +0800

    add 2-8

---
+++ src/t28.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+unsigned rightrot(unsigned x, unsigned n) {
+  while (n > 0) {
+    if ((x & 1) == 1) {
+      x = (x >> 1) | ~(~0U >> 1);
+    } else {
+      x = (x >> 1);
+    }
+    n--;
+  }
+  return x;
+}
+
+int main(void) {
+  unsigned x;
+  int n;
+  for(x = 0; x < 700; x += 49)
+    for(n = 1; n < 8; n++)
+      printf("%u, %d: %u\n", x, n, rightrot(x, n));
+  return 0;
+}
