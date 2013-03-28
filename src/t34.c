7de862d5c8fc25e067c3a161d301f53ccccd4859
lyuehh <lyuehh@gmail.com>
2013-03-28 22:51:22 +0800

    add 3-4

---
+++ src/t34.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char s[]);
+void itoa(int n, char s[]);
+
+int main(int argc, const char *argv[])
+{
+  
+  return 0;
+}
+
+void itoa(int n, char s[]) {
+  int i, sign;
+  if((sign = n) < 0) {
+    n = -n;
+  }
+
+  i = 0;
+  do {
+    s[i++] = n % 10 + '0';
+  } while ((n /= 10) > 0);
+  if(sign < 0) {
+    s[i++] = '-';
+  }
+  s[i] = '\0';
+  reverse(s);
+}
+
+void reverse(char s[]) {
+  int i,j,c;
+  for(i=0, j=strlen(s)-1; i<j;i++,j--) {
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
