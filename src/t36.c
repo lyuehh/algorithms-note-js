2062c594b07cb86ae23dba025b80ca5f4492c48e
lyuehh <lyuehh@gmail.com>
2013-03-29 21:58:07 +0800

    add 3-6

---
+++ src/t36.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char s[]);
+void itoa(int n, char s[], int width);
+
+int main(int argc, const char *argv[])
+{
+  int i = 1235;
+  int i2 = -1235;
+  char s[99];
+  itoa(i, s, 8);
+  printf("%s\n", s);
+  itoa(i2, s, 8);
+  printf("%s\n",s);
+  return 0;
+}
+
+void itoa(int n, char s[], int width) {
+  int i, sign;
+  if((sign = n) < 0) {
+    n = -n;
+  }
+
+  i = 0;
+  do {
+    s[i++] = n % 10 + '0';
+  } while ((n /= 10) > 0);
+
+  if(sign < 0) {
+    s[i++] = '-';
+  }
+
+  while(i < width) {
+    s[i++] = ' ';
+  }
+
+  s[i] = '\0';
+  reverse(s);
+}
+
+void reverse(char s[]) {
+  int i,j,c;
+  for(i=0, j=strlen(s)-1; i<j;i++,j--) {
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
